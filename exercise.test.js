4f060053ae031927f382d2d4cba3ebe1dbc231e8
gregdyke <gregdyke@gmail.com>
2023-03-20 00:30:51 +0000

    initialise with sample exercise

---
+++ exercise.test.js
@@ -0,0 +1,9 @@
+/*
+- write a function canAdmitToNightClub that takes a person object as an argument and returns true if they can be admitted and false if they cannot
+- an unaccompanied minor (under the age of 18) may not be admitted
+- any person who is visibly inebriated may not be admitted
+- the person object should have the following fields
+  - age (Number)
+  - isAccompaniedByAdult (Boolean)
+  - isVisiblyInebriated (Boolean)
+*/
