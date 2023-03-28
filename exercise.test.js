efcfb9cb8e74873b97ea62bd3d8c7a0af79ce309
ChristinaMifsud <christina.mifsud@gmail.com>
2023-03-28 21:36:14 +0100

    Wrote tests
    Wrote tests to test edge cases

--- exercise.test.js
+++ exercise.test.js
@@ -7,3 +7,45 @@
   - isAccompaniedByAdult (Boolean)
   - isVisiblyInebriated (Boolean)
 */
+
+function canAdmitToNightClub(person) {}
+
+// Person A - over 18, not inebriated, no adult
+test("is the person over 18", () => {
+  const personOver18 = {
+    age: 18,
+    isVisiblyInebriated: false,
+    isAccompaniedByAdult: false,
+  };
+  expect(canAdmitToNightClub(personOver18)).toBe(true);
+});
+
+// Person B - under 18, not inebriated, without adult
+test("is the person under 18", () => {
+  const personUnder18 = {
+    age: 17,
+    isVisiblyInebriated: false,
+    isAccompaniedByAdult: false,
+  };
+  expect(canAdmitToNightClub(personUnder18)).toBe(false);
+});
+
+// Person C - under 18, not inebriated, with adult
+test("is the person under 18 with an adult", () => {
+  const personUnder18withAdult = {
+    age: 17,
+    isVisiblyInebriated: false,
+    isAccompaniedByAdult: true,
+  };
+  expect(canAdmitToNightClub(personUnder18withAdult)).toBe(true);
+});
+
+// Person D - over 18, inebriated, without adult
+test("is the person drunk", () => {
+  const personDrunk = {
+    age: 18,
+    isVisiblyInebriated: true,
+    isAccompaniedByAdult: false,
+  };
+  expect(canAdmitToNightClub(personDrunk)).toBe(false);
+});
